20e43c5411c44011c9c9bf8f720393b7d6757a76
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2023-09-08 23:01:45 +0800

    add watchlist

--- Examples/Max.Wpf.Example/MainViewModel.cs
+++ Examples/Max.Wpf.Example/MainViewModel.cs
@@ -8,6 +8,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 using RichillCapital.Max;
+using RichillCapital.Max.Models;
 
 namespace Max.Wpf.Example;
 
@@ -15,22 +16,50 @@ public sealed partial class MainViewModel : ObservableObject
 {
     private readonly MaxDataClient _dataClient;
 
+    [ObservableProperty]
+    private MarketResponse? _selectedMarket = null;
+
     public ObservableCollection<Log> Logs { get; }
+    public ObservableCollection<Log> Trades { get; }
+    public ObservableCollection<MarketResponse> Markets { get; }
     public bool ShouldAutoScroll { get; set; }
 
     public MainViewModel(MaxDataClient dataClient)
     {
         _dataClient = dataClient;
         _dataClient.Connected += _dataClient_Connected;
+        _dataClient.TickerUpdate += _dataClient_TickerUpdate;
+        _dataClient.TradeUpdate += _dataClient_TradeUpdate;
         
         Logs = new();
         BindingOperations.EnableCollectionSynchronization(Logs, new());
+        Markets = new();
+        BindingOperations.EnableCollectionSynchronization(Markets, new());
+        Trades = new();
+        BindingOperations.EnableCollectionSynchronization(Trades, new());
+    }
+
+    private void _dataClient_TradeUpdate(object? sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void _dataClient_TickerUpdate(object? sender, EventArgs e)
+    {
+        throw new NotImplementedException();
     }
 
     private async void _dataClient_Connected(object? sender, EventArgs e)
     {
         AddLog(Log.Info("Connected."));
+
+        Markets.Clear();
         var markets = await _dataClient.GetMarketsAsync();
+        foreach (var market in markets)
+        {
+            Markets.Add(market);
+        }
+        
         AddLog(Log.Info($"Total markets: {markets.Count}"));
     }
 
